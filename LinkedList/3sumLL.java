4e797658a93c2d68304dbb36da4fdb2760cfc254
Pratik Sabata <47950768+pratiksabat@users.noreply.github.com>
2022-07-25 00:00:07 +0530

    3 Sum LL

---
+++ LinkedList/3sumLL.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        
+        for(int i=0;i<nums.length-2;i++){
+            if(i==0 || (i>0&&nums[i]!=nums[i-1])){
+                int s=i+1,e=nums.length-1, sum=0-nums[i];
+                
+                while(s<e){
+                    if(nums[s]+nums[e]==sum){
+                        res.add(Arrays.asList(nums[i],nums[s],nums[e]));
+                        
+                        while(s<e && nums[s]==nums[s+1]) s++;
+                        while(s<e && nums[e]==nums[e-1]) e--;
+                        
+                        s++; e--;
+                    }else if(nums[s]+nums[e]<sum) s++;
+                    else e--;
+                }
+            }
+        }
+        
+        return res;
+    }
+}
